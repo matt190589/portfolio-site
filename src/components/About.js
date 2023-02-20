f08698e3985071834df206ab4ae99531b9efa9f7
matt190589 <matthew.doherty97@gmail.com>
2023-02-20 18:08:47 +0000

    Tailwind used to adjust the screen coverage. Image size adjusted in the about section.

--- src/components/About.js
+++ src/components/About.js
@@ -3,29 +3,13 @@ import React from "react";
 export default function About() {
   return (
     <section id="about">
-      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center h-screen">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-blue-400">
-            Hi, I'm Matt.
-            <br className="hidden lg:inline-block" /> Exploration,
-            Entrepreneurship & Education.
+          <h1 className="title-font sm:text-8xl text-3xl mb-4 font-medium text-blue-400">
+            Exploration, Entrepreneurship & Education.
           </h1>
-          <div className="flex justify-center">
-            <a
-              href="#contact"
-              className="inline-flex text-white bg-blue-400 border-0 py-2 px-6 focus:outline-none hover:bg-blue-200 rounded text-lg"
-            >
-              Work With Me
-            </a>
-            <a
-              href="#projects"
-              className="ml-4 inline-flex text-white bg-blue-400 border-0 py-2 px-6 focus:outline-none hover:bg-blue-200 rounded text-lg"
-            >
-              Explore my projects
-            </a>
-          </div>
         </div>
-        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+        <div className="lg:max-w-2xl lg:w-full md:w-1/2 w-5/6 invisible xl:visible">
           <img
             className="object-cover object-center rounded"
             alt="hero"
