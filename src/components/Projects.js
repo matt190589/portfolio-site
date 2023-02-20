47ed14600e85cda7cb924545d4d346910bd58602
matt190589 <matthew.doherty97@gmail.com>
2023-02-20 19:44:22 +0000

    Images added to the portfolio section showing projects. Consider replacing with banner

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,22 +1,22 @@
-import { CodeIcon } from "@heroicons/react/solid";
+import CodeIcon from "@mui/icons-material/Code";
 import React from "react";
 import { projects } from "../data";
 
 export default function Projects() {
   return (
     <section id="projects" className="text-white bg-blue-400 body-font">
-      <div className="container px-5 py-10 mx-auto text-center lg:px-40">
+      <div className="container px-5 py-10 mx-auto text-center lg:px-40 h-screen flex-col items-center">
         <div className="flex flex-col w-full mb-20">
-          <CodeIcon className="mx-auto inline-block w-10 mb-4" />
+          <CodeIcon className="mx-auto inline-block w-full mb-4" />
           <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
             My Portfolio
           </h1>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-            Through a combination of team projects and personal projects, I have
-            built Apps to develop my skills and develop my Techstack.
+            Through a combination of team and personal projects, I've developed
+            my Techstack and also built Apps people might find useful.
           </p>
         </div>
-        <div className="flex flex-wrap -m-4">
+        <div className="flex flex-wrap m-4 justify-center max-w-full">
           {projects.map((project) => (
             <a
               href={project.link}
@@ -26,10 +26,10 @@ export default function Projects() {
               <div className="flex relative">
                 <img
                   alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center"
+                  className="absolute inset-0 w-full h-full object-contain object-center"
                   src={project.image}
                 />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-yellow-200 bg-blue-400 opacity-0 hover:opacity-100">
+                <div className="px-8 py-10 relative z-10 w-full h-full border-4 border-yellow-200 bg-blue-400 opacity-0 hover:opacity-100">
                   <h2 className="tracking-widest text-sm title-font font-medium text-yellow-200 mb-1">
                     {project.subtitle}
                   </h2>
--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ export const projects = [
     subtitle: "Nextjs, ProgresSQL and Tailwind",
     description:
       "Group project to provide an online App for users of the social prescription scheme. App Front-end and back-end hosted on Netlify.",
-    image: "./project-1.gif",
+    image: "/circles.png",
     link: "https://circlesapp.netlify.app/",
     githubrepo: "https://github.com/SchoolOfCode/circles_app",
   },
@@ -13,7 +13,7 @@ export const projects = [
     subtitle: "Nextjs",
     description:
       "Blog following the week to week changes in fantasy football. First full project with Nextjs, makedown file reading and deployed on vercel.",
-    image: "./project-2.gif",
+    image: "/fantasy_football_blog.png",
     link: "https://fantasy-football-blog.vercel.app/",
     githubrepo: "https://github.com/matt190589/fantasy-football-blog",
   },
@@ -21,8 +21,8 @@ export const projects = [
     title: "Wordle",
     subtitle: "React Native",
     description:
-      "Wordle clone built on React Native using Expo as a framework. UI built with 3D arrays and logic with react hooks. ",
-    image: "./project-3.gif",
+      "Wordle clone built on React Native using Expo as a framework. UI built with 3D arrays and logic with react hooks. App using LocalStorage to limit the need for a live backend. ",
+    image: "/wordle_react_native.png",
     link: "https://reactbootcamp.com",
     githubrepo: "https://github.com/matt190589/wordle-on-react-native",
   },
@@ -30,27 +30,10 @@ export const projects = [
     title: "Reflectify",
     subtitle: "React and PostgresSQL",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-4.gif",
+      "Built for the Tech bootcamper. Sometimes it's difficult to know your learning is going in the right direction. Reflectify is here to help you track your progress.",
+    image: "/personal_portfolio_site.png",
     link: "https://reactbootcamp.com",
     githubrepo: "https://github.com/matt190589/Reflectify",
-  },
-];
-
-export const testimonials = [
-  {
-    quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/1.jpg",
-    name: "Jesse Hicks",
-    company: "Zoozle",
-  },
-  {
-    quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/94.jpg",
-    name: "Ruben Alvarez",
-    company: "Dooble.io",
   },
 ];
 
