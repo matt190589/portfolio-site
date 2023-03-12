415b4f57fb1de3b59978278bcc1bd668f9b2ccb6
matt190589 <matthew.doherty97@gmail.com>
2023-03-12 22:21:46 +0000

    Projects component removed

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import React from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
-import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Profile from "./components/Profile";
-import CarouselItem from "./components/Carousel";
 import PracticeCarousel from "./components/PracticeCarousel";
 
 export default function App() {
@@ -13,12 +11,10 @@ export default function App() {
     <main className="text-blue-400 body-font">
       <Navbar />
       <About />
-      <Projects />
-      {/* <CarouselItem /> */}
+      <PracticeCarousel />
       <Skills />
       <Profile />
       <Contact />
-      <PracticeCarousel />
     </main>
   );
 }
--- src/components/PracticeCarousel.js
+++ src/components/PracticeCarousel.js
@@ -23,7 +23,7 @@ function PracticeCarousel() {
   };
 
   return (
-    <div className="relative container px-5 py-10 mx-auto text-center lg:px-40 h-screen">
+    <div className="relative container px-5 py-10 mx-auto lg:px-40">
       <div className="relative flex justify-center items-center">
         <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center opacity-0 transition duration-500 ease-in-out hover:opacity-100 z-30 object-cover">
           <div
@@ -42,7 +42,7 @@ function PracticeCarousel() {
         <img
           className="flex items-center justify-center border border-yellow-200 rounded-m object-contain"
           src={projects[currentIndex].url}
-          style={{ width: "80%", height: "50%" }}
+          style={{ width: "80%" }}
         />
       </div>
       <div className="flex top-4 justify-center py-2">
@@ -62,42 +62,7 @@ function PracticeCarousel() {
 
 export default PracticeCarousel;
 
-//  <div className="max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group">
-//       <div className="flex flex-col w-full mb-10">
-//         <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-blue-400">
-//           My Portfolio
-//         </h1>
-//         <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-//           Through a combination of team and personal projects, I've developed my
-//           Techstack and also built Apps people might find useful.
-//         </p>
-//       </div>
-//       <div className="flex items-center justify-center w-full h-full rounded-2xl duration-500 z-auto">
-//         <div className="border-4 border-yellow-200 bg-blue-400 opacity-0 hover:opacity-100 z-30">
-//           <h2 className="tracking-widest text-sm title-font font-medium text-yellow-200 mb-1">
-//             {slides[currentIndex].subtitle}
-//           </h2>
-//           <h1 className="title-font text-lg font-medium text-white mb-3">
-//             {slides[currentIndex].title}
-//           </h1>
-//           <p className="leading-relaxed">{slides[currentIndex].description}</p>
-//         </div>
-//         <img
-//           className="inset-0 w-full h-full border border-yellow-200 rounded-md"
-//           src={slides[currentIndex].url}
-//           style={{ maxWidth: "1000px", maxHeight: "600px" }}
-//         />
-//       </div>
-//       <div className="flex top-4 justify-center py-2">
-//         {slides.map((slide, slideIndex) => (
-//           <div
-//             key={slideIndex}
-//             onClick={() => goToSlide(slideIndex)}
-//             className="text-2xl cursor-pointer"
-//           >
-//             <RxDotFilled />
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
+//Add a ref
+{
+  /* <a href={project.link} key={project.image} className="sm:w-1/2 w-100 p-4"> */
+}
